40b030e857693b61e703c9cfd734b180da9e9538
sbhartia3101 <sweetybhartia94@gmail.com>
2019-05-11 23:00:34 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account = "ACd29dce7470e6ec135d024c91579ae96b"
-token = "0dbb6d285298369e081e93bfc2f9aae5"
+account = {{twilio-account}}
+token = {{twilio-token}}
 client = Client(account, token)
 
-binding1 = '{"binding_type":"sms","address":"+19174787238"}'
+binding1 = '{"binding_type":"sms","address":{{twilio-number}}}'
 notification = client.notify.services("IS7ab0037fdef46943ed6829ac36f6a9fc") \
-    .notifications.create(to_binding=[binding1], body="Hello Sweety")
+    .notifications.create(to_binding=[binding1], body="Hello")
 
 print(notification)
